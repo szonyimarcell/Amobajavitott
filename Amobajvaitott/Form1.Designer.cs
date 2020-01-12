b71703b769e931c027e3def24b8acc99d11922d3
szonyimarcell <57617010+szonyimarcell@users.noreply.github.com>
2020-01-12 19:20:05 +0100

    Add files via upload

---
+++ Amobajvaitott/Form1.Designer.cs
@@ -0,0 +1,326 @@
+﻿namespace Amobajvaitott
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button14 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button10 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 158);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 13);
+            this.label4.TabIndex = 81;
+            this.label4.Text = "Port:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 80;
+            this.label3.Text = "IP cím:";
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(12, 12);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(100, 42);
+            this.button14.TabIndex = 78;
+            this.button14.Text = "Host leszek";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 174);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 73;
+            this.textBox2.Text = "23";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 134);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 72;
+            this.textBox1.Text = "localhost";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 13);
+            this.label1.TabIndex = 71;
+            this.label1.Text = "Csatlakozás szerverhez:";
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(12, 60);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(100, 42);
+            this.button11.TabIndex = 70;
+            this.button11.Text = "csatlakozás";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button9
+            // 
+            this.button9.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 50.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.Location = new System.Drawing.Point(165, 168);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 75);
+            this.button9.TabIndex = 69;
+            this.button9.Tag = "9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button8
+            // 
+            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 50.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.Location = new System.Drawing.Point(84, 168);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 75);
+            this.button8.TabIndex = 68;
+            this.button8.Tag = "8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button7
+            // 
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 50.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(3, 168);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 75);
+            this.button7.TabIndex = 67;
+            this.button7.Tag = "7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button6
+            // 
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 50.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(165, 87);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 75);
+            this.button6.TabIndex = 66;
+            this.button6.Tag = "6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button5
+            // 
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 50.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(84, 87);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 75);
+            this.button5.TabIndex = 65;
+            this.button5.Tag = "5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button4
+            // 
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 50.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(3, 87);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 75);
+            this.button4.TabIndex = 64;
+            this.button4.Tag = "4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 50.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(165, 6);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 75);
+            this.button3.TabIndex = 63;
+            this.button3.Tag = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 50.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(84, 6);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 75);
+            this.button2.TabIndex = 62;
+            this.button2.Tag = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 50.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(3, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 75);
+            this.button1.TabIndex = 60;
+            this.button1.Tag = "1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.button7);
+            this.panel1.Controls.Add(this.button8);
+            this.panel1.Controls.Add(this.button9);
+            this.panel1.Enabled = false;
+            this.panel1.Location = new System.Drawing.Point(267, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(245, 247);
+            this.panel1.TabIndex = 82;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(156, 12);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(61, 42);
+            this.button10.TabIndex = 83;
+            this.button10.Text = "Indítás";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(153, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 84;
+            this.label2.Text = "label2";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(194, 75);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 70;
+            this.label5.Text = "label5";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(514, 251);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button11);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Amobajvaitott/Form1.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Amobajvaitott
+{
+    public partial class Form1 : Form
+    {
+        private string sajat;
+        private string ellenfel;
+        private int sajat_pont = 0;
+        private int ellenfel_pont = 0;
+        byte[] num = { 0 };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        TcpClient clientSocket;
+        private bool jatekagoritmus()
+        {
+            //vizszintes
+            if (button1.Text == button2.Text && button2.Text == button3.Text && button3.Text != "")
+            {
+                if (button3.Text == sajat)
+                {
+                    sajat_pont += 1;
+                    MessageBox.Show(sajat + ": játékos győzedelmeskedett");
+                    label2.Text = sajat + " " + sajat_pont;
+                    label5.Text = ellenfel + " " + ellenfel_pont;
+                    inditas();;
+                }
+                else
+                {
+                    ellenfel_pont += 1;
+                    MessageBox.Show(ellenfel + ": játékos győzedelmeskedett");
+                    label2.Text = sajat + " " + sajat_pont;
+                    label5.Text = ellenfel + " " + ellenfel_pont;
+                    inditas();;
+                }
+                return true;
+            }
+            else if (button4.Text == button5.Text && button5.Text == button6.Text && button6.Text != "")
+            {
+                if (button6.Text == sajat)
+                {
+                    sajat_pont += 1;
+                    MessageBox.Show(sajat + ": játékos győzedelmeskedett");
+                    label2.Text = sajat + " " + sajat_pont;
+                    label5.Text = ellenfel + " " + ellenfel_pont;
+                    inditas();;
+                }
+                else
+                {
+                    ellenfel_pont += 1;
+                    MessageBox.Show(ellenfel + ": játékos győzedelmeskedett");
+                    label2.Text = sajat + " " + sajat_pont;
+                    label5.Text = ellenfel + " " + ellenfel_pont;
+                    inditas();;
+                }
+                return true;
+            }
+            else if (button7.Text == button8.Text && button8.Text == button9.Text && button9.Text != "")
+            {
+                if (button9.Text == sajat)
+                {
+                    sajat_pont += 1;
+                    MessageBox.Show(sajat + ": játékos győzedelmeskedett");
+                    label2.Text = sajat + " " + sajat_pont;
+                    label5.Text = ellenfel + " " + ellenfel_pont;
+                    inditas();;
+                }
+                else
+                {
+                    ellenfel_pont += 1;
+                    MessageBox.Show(ellenfel + ": játékos győzedelmeskedett");
+                    label2.Text = sajat + " " + sajat_pont;
+                    label5.Text = ellenfel + " " + ellenfel_pont;
+                    inditas();;
+                }
+                return true;
+            }
+            //fuggoleges
+            else if (button1.Text == button4.Text && button4.Text == button7.Text && button7.Text != "")
+            {
+                if (button7.Text == sajat)
+                {
+                    sajat_pont += 1;
+                    MessageBox.Show(sajat + ": játékos győzedelmeskedett");
+                    label2.Text = sajat + " " + sajat_pont;
+                    label5.Text = ellenfel + " " + ellenfel_pont;
+                    inditas();;
+                }
+                else
+                {
+                    ellenfel_pont += 1;
+                    MessageBox.Show(ellenfel + ": játékos győzedelmeskedett");
+                    label2.Text = sajat + " " + sajat_pont;
+                    label5.Text = ellenfel + " " + ellenfel_pont;
+                    inditas();;
+                }
+                return true;
+            }
+            else if (button2.Text == button5.Text && button5.Text == button8.Text && button8.Text != "")
+            {
+                if (button8.Text == sajat)
+                {
+                    sajat_pont += 1;
+                    MessageBox.Show(sajat + ": játékos győzedelmeskedett");
+                    label2.Text = sajat + " " + sajat_pont;
+                    label5.Text = ellenfel + " " + ellenfel_pont;
+                    inditas();;
+                }
+                else
+                {
+                    ellenfel_pont += 1;
+                    MessageBox.Show(ellenfel + ": játékos győzedelmeskedett");
+                    label2.Text = sajat + " " + sajat_pont;
+                    label5.Text = ellenfel + " " + ellenfel_pont;
+                    inditas();;
+                }
+                return true;
+            }
+            else if (button3.Text == button6.Text && button6.Text == button9.Text && button9.Text != "")
+            {
+                if (button9.Text == sajat)
+                {
+                    sajat_pont += 1;
+                    MessageBox.Show(sajat + ": játékos győzedelmeskedett");
+                    label2.Text = sajat + " " + sajat_pont;
+                    label5.Text = ellenfel + " " + ellenfel_pont;
+                    inditas();;
+                }
+                else
+                {
+                    ellenfel_pont += 1;
+                    MessageBox.Show(ellenfel + ": játékos győzedelmeskedett");
+                    label2.Text = sajat + " " + sajat_pont;
+                    label5.Text = ellenfel + " " + ellenfel_pont;
+                    inditas();;
+                }
+                return true;
+            }
+            //átlós
+            else if (button1.Text == button5.Text && button5.Text == button9.Text && button9.Text != "")
+            {
+                if (button9.Text == sajat)
+                {
+                    sajat_pont += 1;
+                    MessageBox.Show(sajat + ": játékos győzedelmeskedett");
+                    label2.Text = sajat + " " + sajat_pont;
+                    label5.Text = ellenfel + " " + ellenfel_pont;
+                    inditas();;
+                }
+                else
+                {
+                    ellenfel_pont += 1;
+                    MessageBox.Show(ellenfel + ": játékos győzedelmeskedett");
+                    label2.Text = sajat + " " + sajat_pont;
+                    label5.Text = ellenfel + " " + ellenfel_pont;
+                    inditas();
+                }
+                return true;
+            }
+            else if (button3.Text == button5.Text && button5.Text == button7.Text && button7.Text != "")
+            {
+                if (button7.Text == sajat)
+                {
+                    sajat_pont += 1;
+                    MessageBox.Show(sajat + ": játékos győzedelmeskedett");
+                    label2.Text = sajat + " " + sajat_pont;
+                    label5.Text = ellenfel + " " + ellenfel_pont;
+                    inditas();
+                }
+                else
+                {
+                    ellenfel_pont += 1;
+                    MessageBox.Show(ellenfel + ": játékos győzedelmeskedett");
+                    label2.Text = sajat + " " + sajat_pont;
+                    label5.Text = ellenfel + " " + ellenfel_pont;
+                    inditas();
+                }
+                return true;
+            }
+            //döntetlen
+            else if (button1.Text != "" && button2.Text != "" && button3.Text != "" && button4.Text != "" && button5.Text != "" && button6.Text != "" && button7.Text != "" && button8.Text != "" && button9.Text != "")
+            {
+                MessageBox.Show("Döntetlen");
+                inditas();
+                return true;
+            }
+            return false;
+
+        }
+        private void fogadas()
+        {
+            clientSocket.Client.Receive(num);
+            if (num[0] == 1)
+            {
+                button1.Text = ellenfel.ToString();
+                button1.Enabled = false;
+            }
+            if (num[0] == 2)
+            {
+                button2.Text = ellenfel.ToString();
+                button2.Enabled = false;
+            }
+            if (num[0] == 3)
+            {
+                button3.Text = ellenfel.ToString();
+                button3.Enabled = false;
+            }
+            if (num[0] == 4)
+            {
+                button4.Text = ellenfel.ToString();
+                button4.Enabled = false;
+            }
+            if (num[0] == 5)
+            {
+                button5.Text = ellenfel.ToString();
+                button5.Enabled = false;
+            }
+            if (num[0] == 6)
+            {
+                button6.Text = ellenfel.ToString();
+                button6.Enabled = false;
+            }
+            if (num[0] == 7)
+            {
+                button7.Text = ellenfel.ToString();
+                button7.Enabled = false;
+            }
+            if (num[0] == 8)
+            {
+                button8.Text = ellenfel.ToString();
+                button8.Enabled = false;
+            }
+            if (num[0] == 9)
+            {
+                button9.Text = ellenfel.ToString();
+                button9.Enabled = false;
+            }
+        }
+        private void inditas()
+        {
+            panel1.Enabled = true;
+            button1.Text = "";
+            button2.Text = "";
+            button3.Text = "";
+            button4.Text = "";
+            button5.Text = "";
+            button6.Text = "";
+            button7.Text = "";
+            button8.Text = "";
+            button9.Text = "";
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+            button.Text = sajat;
+            button.Enabled = false;
+            num[0] = byte.Parse(button.Tag as string);
+            clientSocket.Client.Send(num);
+            fogadas();
+            jatekagoritmus();
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            clientSocket = new TcpClient();
+            clientSocket.Connect(textBox1.Text, int.Parse(textBox2.Text));
+            sajat = "O";
+            ellenfel = "X";
+            inditas();
+            fogadas();
+            textBox1.Enabled = false;
+            textBox2.Enabled = false;
+            button11.Enabled = false;
+            button14.Enabled = false;
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            TcpListener host = new TcpListener(new IPEndPoint(IPAddress.Any, int.Parse(textBox2.Text)));
+            host.Start();
+            sajat = "X";
+            ellenfel = "O";
+            clientSocket = host.AcceptTcpClient();
+            inditas();
+            textBox1.Enabled = false;
+            textBox2.Enabled = false;
+            button11.Enabled = false;
+            button14.Enabled = false;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            inditas();
+        }
+    }
+}
